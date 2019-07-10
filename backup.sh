e450d2bcae2114bf9265b7e3c2be2ad2f4f6dc94
Tomas Herceg <tth@rfa.cz>
2019-07-10 12:33:17 +0200

    red stderr in interactive mode
    Signed-off-by: Tomas Herceg <tth@rfa.cz>

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@ for host in ${HOSTS[@]}; do
 	lockfile -r 0 $LOCK_DIR/$host || continue
 	options=""
 	debug "backuping host" "${White}${host}"
-	/sbin/zfs create -p $POOL/$host
+	/sbin/zfs create -p $POOL/$host 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 	[ -r ${EXCLUDE_DIR}/${host} ] && options="$options --exclude-from ${EXCLUDE_DIR}/${host}"
 	[ $DEBUG_RSYNC == 1 ] && options="$options -vP --human-readable"
 	i=0
@@ -20,24 +20,24 @@ for host in ${HOSTS[@]}; do
 		debug "rsync loop try" "$i"
 		rsync_command="rsync -aAX $options --bwlimit $BW_LIMIT --numeric-ids --exclude-from $EXCLUDE_PATH --delete --hard-links --inplace --delete-excluded $host:/ $MOUNT_DIR/$host"
 		debug "rsync command" "$rsync_command"
-		$rsync_command
-	       	status=$?
+		$rsync_command 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
+		status=$?
 		debug "rsync status" "$status"
 	done
 	debug "rsync loop end" "$i $status"
 	if [ $status -eq 0 ]; then
 		snap_command="/sbin/zfs snap ${POOL}/${host}@$(date +'%Y-%m-%d-%H:%M:%S')"
 		debug "creating snapshot" "$snap_command"
-		$snap_command
-		touch $MOUNT_DIR/$host/.last_backup
+		$snap_command 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
+		touch $MOUNT_DIR/$host/.last_backup 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 	else
 		debug "rsync failed, skipping create snapshot"
 	fi
 	rem_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_DAYS days" +"%Y%m%d") -F '[ @-]' '/$host/ {date=\$2\$3\$4; count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if ( key <= keep) { for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
 	debug "deleting all older than $KEEP_DAYS days old" "$rem_old"
-	eval $rem_old
+	eval $rem_old 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 	rem_more_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_ALL_DAYS days" +"%Y%m%d") -F '[ @-]' '/$host/ {date=\$2\$3\$4; count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if (count[key] > 1 && key <= keep) { delete snap[key][count[key]]; for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
 	debug "keep only one snapshot older than $KEEP_ALL_DAYS days per day" "$rem_more_old"
-	eval $rem_more_old
-	rm -f $LOCK_DIR/$host
+	eval $rem_more_old 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
+	rm -f $LOCK_DIR/$host 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 done
--- delete.sh
+++ delete.sh
@@ -10,9 +10,9 @@ for host in ${HOSTS[@]}; do
 	lockfile -r 0 $LOCK_DIR/$host || continue
 	rem_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_DAYS days" +"%Y%m%d") -F '[ @-]' '/$host/ {date=\$2\$3\$4; count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if ( key <= keep) { for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
 	debug "deleting all older than $KEEP_DAYS days old" "$rem_old"
-	eval $rem_old
+	eval $rem_old 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 	rem_more_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_ALL_DAYS days" +"%Y%m%d") -F '[ @-]' '/$host/ {date=\$2\$3\$4; count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if (count[key] > 1 && key <= keep) { delete snap[key][count[key]]; for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
 	debug "keep only one snapshot older than $KEEP_ALL_DAYS days per day" "$rem_more_old"
-	eval $rem_more_old
-	rm -f $LOCK_DIR/$host
+	eval $rem_more_old 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
+	rm -f $LOCK_DIR/$host 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 done

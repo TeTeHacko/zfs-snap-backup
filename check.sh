83960fa53785115a3d83775b671cd80bbcb41984
Tomas Herceg <tth@rfa.cz>
2019-11-21 00:09:15 +0100

    make check script nagios friendly and some variables chanble at runtime
    Signed-off-by: Tomas Herceg <tth@rfa.cz>

--- check.sh
+++ check.sh
@@ -6,24 +6,49 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/shared.sh
 
+prob=0
+declare -A crit
+declare -A warn
+
 for host in ${HOSTS[@]}; do
 	debug "checking host" "${White}${host}"
 
   ret=$(ssh -o ConnectTimeout=5 -o PasswordAuthentication=no ${host} true 2>&1)
   rc=$?
+  prob=$((prob + rc))
   if [ $rc -gt 0 ]; then
-    debug "${Red}${ret}"
+    debug "${Red}crit" "${ret}"
+    crit["${host}"]="${ret}"
   fi
 
   ret=$(./check_backup.sh "${host}" 2>&1)
   rc=$?
+  prob=$((prob + rc))
   if [ $rc -gt 1 ]; then
-    debug "${Red}${ret}"
+    debug "${Red}crit" "${ret}"
+    crit["${host}"]=""
   elif [ $rc -eq 1 ]; then
-    debug "${Orange}${ret}"
+    debug "${Orange}warn" "${ret}"
+    warn["${host}"]=""
   else
-    debug "${Green}${ret}"
+    debug "${Green}ok" "${ret}"
   fi
-
-  echo
 done
+
+if [ ${#warn[@]} -gt 0 ]; then
+  echo -n "WARNING: "
+  rc=1
+  for host in "${!warn[@]}"; do
+    echo -n "${host}, "
+  done
+fi
+if [ ${#crit[@]} -gt 0 ]; then
+  echo -n "CRITICAL: "
+  rc=2
+  for host in "${!crit[@]}"; do
+    echo -n "${host}, "
+  done
+fi
+
+echo
+exit $rc

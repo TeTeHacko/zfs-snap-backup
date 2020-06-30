4b9b50bd40249c22f44bc5f185d14ebcd4da1c28
TeTeHacko <tth@rfa.cz>
2020-06-30 10:19:00 +0200

    better awk to correctly support hostnames with -
    Signed-off-by: TeTeHacko <tth@rfa.cz>

--- backup.sh
+++ backup.sh
@@ -44,10 +44,10 @@ for host in ${HOSTS[@]}; do
 	else
 		debug "rsync failed, skipping create snapshot"
 	fi
-	rem_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_DAYS days" +"%Y%m%d") -F '[ @-]' '/$host/ {date=\$2\$3\$4; count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if ( key <= keep) { for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
+	rem_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_DAYS days" +"%Y%m%d") -F '[ @]' '/$host/ {date=substr(\$2,1,4) substr(\$2,6,2) substr(\$2,9,2); count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if ( key <= keep) { for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
 	debug "deleting all older than $KEEP_DAYS days old" "$rem_old"
 	eval $rem_old 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
-	rem_more_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_ALL_DAYS days" +"%Y%m%d") -F '[ @-]' '/$host/ {date=\$2\$3\$4; count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if (count[key] > 1 && key <= keep) { delete snap[key][count[key]]; for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
+	rem_more_old="/sbin/zfs list -t snapshot -o name -H |awk -v keep=$(date -d "now -$KEEP_ALL_DAYS days" +"%Y%m%d") -F '[ @]' '/$host/ {date=substr(\$2,1,4) substr(\$2,6,2) substr(\$2,9,2); count[date]++; snap[date][count[date]]=\$0} END { for (key in count) { if (count[key] > 1 && key <= keep) { delete snap[key][count[key]]; for ( prt in snap[key] ) { print snap[key][prt] }}} }' | xargs -r -n 1 /sbin/zfs destroy"
 	debug "keep only one snapshot older than $KEEP_ALL_DAYS days per day" "$rem_more_old"
 	eval $rem_more_old 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
 	rm -f $LOCK_DIR/$host 2> >(while read line; do echo -e "${Red}${line}${Reset}" >&2; done)
